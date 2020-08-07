622d769cdf356fcf80de61c4c1743acd8be5dd72
Brandon <vagarioustoast@gmail.com>
2020-08-07 15:00:27 -0700

    adds infinite loading and loader icon

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 const imageContainer = document.getElementById("image-container");
 const loader = document.getElementById("loader");
 
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
 let photosArray = [];
 
 // Unsplash API
-const count = 10;
+const count = 30;
 const apiKey = "SUvMs7ekfLrPhl3E-o_ued5k3_0uyOxjW1j32oV-6Vc";
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+// Check if all images were loaded
+function imageLoaded() {
+  imagesLoaded++;
+  if (imagesLoaded === totalImages) {
+    ready = true;
+    loader.hidden = true;
+    console.log(`ready = ${ready}`);
+  }
+}
 
 // Helper Function to Set Attributes on DOM Elements
 function setAttributes(element, attributes) {
@@ -17,6 +30,9 @@ function setAttributes(element, attributes) {
 
 // Create Elements for Links and Photos, Add to DOM
 function displayPhotos() {
+  imagesLoaded = 0;
+  totalImages = photosArray.length;
+  console.log(`total images ${totalImages}`);
   // Run function for each object in Array
   photosArray.forEach((photo) => {
     // Create <a> to link to Unsplash
@@ -32,6 +48,8 @@ function displayPhotos() {
       alt: photo.alt_description,
       title: photo.alt_description,
     });
+    // Event Listener, check when each is finished loading
+    img.addEventListener("load", imageLoaded);
     // Put <img> inside <a>, then put both inside imageContainer Element
     item.appendChild(img);
     imageContainer.appendChild(item);
@@ -48,6 +66,17 @@ async function getPhotos() {
     // Catch Error here
   }
 }
+
+// Check to see if scrolling near the bottom of page, Load More Photos
+window.addEventListener("scroll", () => {
+  if (
+    window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 &&
+    ready
+  ) {
+    ready = false;
+    getPhotos();
+  }
+});
 
 // On Load
 getPhotos();
